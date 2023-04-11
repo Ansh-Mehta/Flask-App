eb811d014aac672c1c985df8ac0052033c33a6da
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2023-04-11 23:28:15 +0530

    updated code

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/details/")
+def details():
+    return render_template("details.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
